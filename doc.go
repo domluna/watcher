dbd4bf9ee8f954dc1ad3fc274eb3f7f1ab0829a6
Dominique Luna <dluna132@gmail.com>
2014-10-29 17:45:37 -0400

    doc

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+Watcher provides a way to be notified of changes to files
+starting from the root of a given directory. Folder paths
+to ignore can be customized to fit needs.
+
+NOTE: Watcher does NOT handle filtering messages. It delegates
+this to the user for maximum flexibility.
+*/
+package watcher
